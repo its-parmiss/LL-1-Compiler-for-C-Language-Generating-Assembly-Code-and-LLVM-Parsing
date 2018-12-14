8bbc01838566fb6e5bb8ec360ff9dde42131c851
Parmiss <parmiss.sh98@gmail.com>
2018-12-14 12:56:30 +0330

    parser

--- src/Token.java
+++ src/Token.java
@@ -1,14 +1,19 @@
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by saeed on 10/9/2018 AD.
  */
 enum Type{
-    id,number,ST,string,comment,KW,EOF;
+    id,number,ST,string,comment,KW,EOF,Terminal,Variable;
 }
-public class Token {
+public class Token  implements Comparable<Token>{
     Type type;
     String value;
+    String typeString;
     public Token(String type,String value)
     {
+        typeString=type;
         switch (type) {
             case "EOF":
                 this.type = Type.EOF;
@@ -32,12 +37,25 @@ public class Token {
                 case "KW":
                 this.type = Type.KW;
                 break;
+            case "Terminal":
+                this.type=Type.Terminal;
+                break;
+            case "Variable":
+                this.type=Type.Variable;
+                break;
         }
         this.value = value;
+
     }
 
-
+    @Override
+    public int compareTo(Token o) {
+        if(o.value.equals(this.value) && o.typeString.equals(this.typeString)){
+            return 0;
+        }
+        return -1;
+    }
     public void getToken(){
-        System.out.println("<" + type + "," +"'"+ value +"'"+ ">");
+        System.out.print(" <" + type + "," +"'"+ value +"'"+ "> ");
     }
 }
